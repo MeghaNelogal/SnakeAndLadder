2d88de0aa37ac594cf892fa5bf7248a18f5c5f1c
Your Name <you@example.com>
2022-04-10 13:00:50 -0700

    [Megha] Add: Two Player Game Is Executed

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,14 +1,58 @@
-﻿using System;
-namespace SnakeAndLadder
-{
-    class Program
-    {
-        static void Main(String[] args)
-        {
-            SnakeAndLadderGame game = new SnakeAndLadderGame();
-            Console.WriteLine("The Die rolled number is " + game.DieRoll());
-            game.Game();
-        }
+﻿using SnakeAndLadder;
+Console.WriteLine("SNAKE AND LADDER GAME");
 
+SnakeAndLadderGame game = new SnakeAndLadderGame();
+game.start();
+public class Player
+{
+    public int position = 0;
+    public static Random random = new Random();
+    public int roll;
+    public int totalRoll;
+    public void Move(int movement)
+    {
+        position += movement;
+        if (position > 100)
+        {
+            position -= movement;
+            Console.WriteLine("You crossed 100 so moving back to last position");
+        }
+    }
+    public void RollDice()
+    {
+        roll = random.Next(1, 7);
+        totalRoll++;
+        Display();
+        Console.WriteLine("Player rolled: " + roll);
+        Move(roll);
+        Option();
+    }
+    public void Display()
+    {
+        Console.WriteLine("Player position: " + position);
+    }
+    public void Option()
+    {
+        int option = random.Next(0, 3);
+        switch (option)
+        {
+            case 0:
+                Console.WriteLine("No Play");
+                break;
+
+            case 1:
+                Console.WriteLine("You are on Snake");
+                Move(-roll);
+                break;
+
+            case 2:
+                Console.WriteLine("You found a ladder");
+                Move(roll);
+                break;
+
+            default:
+                Console.WriteLine("Error");
+                break;
+        }
     }
 }
