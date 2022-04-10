d928cc066091ae0fdbd19c212401beafdf54501a
Your Name <you@example.com>
2022-04-10 12:26:59 -0700

    [Megha] Add: Single Player Starts The Game

--- SnakeAndLadder/SnakeAndLadderGame.cs
+++ SnakeAndLadder/SnakeAndLadderGame.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace SnakeAndLadder
 {
-    internal class SnakeAndLadderGame
+    public class SnakeAndLadderGame
     {
+        int positionZero = 0;
     }
 }
